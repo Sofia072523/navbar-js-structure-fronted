16a8e702137f7a4627810ad9173680dc39b23fb4
Sofia072523 <karensofia040421@gmail.com>
2025-08-26 16:07:17 -0500

    style: se agregarón estilos a las cards

--- frontend/public/js/components/card.js
+++ frontend/public/js/components/card.js
@@ -5,11 +5,11 @@ export async function loadCards(containerSelector, cardIds = []) {
 
     //Obtenemos el contenedor del DOM
     const container = document.querySelector(containerSelector);
-    
-    if(!container)return; //Si no exite simplemente nos salimos
 
-    try{
-        const[templateRes, dataRes] = await Promise.all([
+    if (!container) return; //Si no exite simplemente nos salimos
+
+    try {
+        const [templateRes, dataRes] = await Promise.all([
             //Hacer 2 fetch al mismo tiempo
             //1- Es para la plantilla
             //2- Es para los datos
@@ -23,23 +23,26 @@ export async function loadCards(containerSelector, cardIds = []) {
 
         //Filtramos las cards si se proporcina los IDs específicos
         const filteredCards = cardIds.length
-        ? cards.filter(card => cardIds.includes(card.id)) //Solo las que están en el arreglo
-        :cards; //Si no hay filtro uselas todas
+            ? cards.filter(card => cardIds.includes(card.id)) //Solo las que están en el arreglo
+            : cards; //Si no hay filtro uselas todas
 
-        filteredCards.forEach(card =>{
+        filteredCards.forEach(card => {
 
             //Remplazar los pacecholder {{...}} del template con los datos reales
             let html = template
-            .replace("{{title}}",card.title)
-            .replace("{{icon1}}",card.icon1)
-            .replace("{{icon2}}",card.icon2)
-            .replace("{{description}}",card.description)
+                .replace("{{titulo}}", card.title)
+                .replace("{{icon1}}", card.icon1)
+                .replace("{{icon2}}", card.icon2)
+                .replace("{{description}}", card.description)
+            
+            
+            //container.innerHTML += html;
+            container.insertAdjacentHTML("beforeend", html);
 
-            container.innerHTML += html;
         });
     }
-    
-    catch(error){
+
+    catch (error) {
         console.error("Error cargando las cards", error)
     }
 }
